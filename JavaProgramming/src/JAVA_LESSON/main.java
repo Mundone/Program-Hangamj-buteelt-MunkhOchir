e5c30e9ea3bca44408d0ef717e03227aea36217e
hp <hp@DESKTOP-CD499H8>
2022-02-11 16:09:52 +0800

    Check git

---
+++ JavaProgramming/src/JAVA_LESSON/main.java
@@ -0,0 +1,17 @@
+package JAVA_LESSON;
+
+public class main {
+ 
+	public static void main(String[] args)
+	{
+		int i = 2;
+		float f = 5.5f;
+		String s = "String";
+		char ch = 'c';
+		System.out.println("int: " + i);
+		System.out.println("float: " + f);
+		System.out.println("String: " + s);
+		System.out.println("char: " + ch);
+		Integer.parseInt(s);
+	}
+}
